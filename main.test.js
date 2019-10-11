6cb2bee243d7ef89eb04e84d5349779bc7d57bf7
prasu93 <prasannamahesh93@gmail.com>
2019-10-11 11:37:34 -0500

    adding qunit part

---
+++ main.test.js
@@ -0,0 +1,13 @@
+QUnit.module('MAIN MODULE', {})  // group all these tests together
+
+QUnit.test('Time to pass', assert => {
+    
+    assert.equal(samespeed(10,5),"5", "Perfect result")
+    assert.equal(oppspeed(10,5),"15", "Perfect result")
+    assert.equal(samespeed(60,6),"54", "Perfect result")
+    assert.equal(oppspeed(60,6),"66", "Perfect result")
+    assert.equal(samespeed(10,5),"6", "Incorrect result")
+})
+
+
+
