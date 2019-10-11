3cfcf25531679d8067c34903dc7356355016e87d
prasu93 <prasannamahesh93@gmail.com>
2019-10-11 15:37:40 -0500

    Edited test script

--- main.test.js
+++ main.test.js
@@ -6,7 +6,7 @@ QUnit.test('Time to pass', assert => {
     assert.equal(oppspeed(10,5),"15", "Perfect result")
     assert.equal(samespeed(60,6),"54", "Perfect result")
     assert.equal(oppspeed(60,6),"66", "Perfect result")
-    assert.equal(samespeed(10,5),"6", "Incorrect result")
+    assert.equal(samespeed(-10,5),"Speed can't be negative", "Incorrect result")
 })
 
 
